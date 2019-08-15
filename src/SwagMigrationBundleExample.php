35d757cad69074d00ef78954eff395787262435a
Michael Telgmann <mt@shopware.com>
2019-08-15 15:58:06 +0200

    Check if Migration plugin is in system and active before adding migration relevant services to the DI container

--- src/SwagMigrationBundleExample.php
+++ src/SwagMigrationBundleExample.php
@@ -3,8 +3,24 @@
 namespace SwagMigrationBundleExample;
 
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Kernel;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class SwagMigrationBundleExample extends Plugin
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
 
+        $migrationPlugin = Kernel::getPlugins()->get(\SwagMigrationAssistant\SwagMigrationAssistant::class);
+        if ($migrationPlugin === null || $migrationPlugin->isActive() === false) {
+            return;
+        }
+
+        // Only load migration relevant classes if the SwagMigrationAssistant is available
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/DependencyInjection/'));
+        $loader->load('migration_assistant_extension.xml');
+    }
 }
