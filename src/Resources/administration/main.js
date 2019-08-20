c3c7ce6332b13438b3fae44caa40d9d1c947d8a9
Stephan Pohl <stp@shopware.com>
2019-08-20 15:35:46 +0200

    Use third party interface

--- src/Resources/administration/main.js
+++ src/Resources/administration/main.js
@@ -1,6 +1,7 @@
-import { Application } from 'src/core/shopware';
 import deDeSnippets from './snippet/de-DE.json';
 import enGBSnippets from './snippet/en-GB.json';
+
+const { Application } = Shopware;
 
 Application.addInitializerDecorator('locale', (localeFactory) => {
     localeFactory.extend('de-DE', deDeSnippets);
