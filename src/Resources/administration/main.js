c46ab2f89745ad41c189264d9c7c6a81b0e2a5d7
Jannis Leifeld <j.leifeld@shopware.com>
2019-10-24 07:49:44 +0200

    Update snippet extension with new syntax

--- src/Resources/administration/main.js
+++ src/Resources/administration/main.js
@@ -1,10 +1,5 @@
-import { Application } from 'src/core/shopware';
 import deDeSnippets from './snippet/de-DE.json';
 import enGBSnippets from './snippet/en-GB.json';
 
-Application.addInitializerDecorator('locale', (localeFactory) => {
-    localeFactory.extend('de-DE', deDeSnippets);
-    localeFactory.extend('en-GB', enGBSnippets);
-
-    return localeFactory;
-});
+Shopware.Locale.extend('de-DE', deDeSnippets);
+Shopware.Locale.extend('en-GB', enGBSnippets);
